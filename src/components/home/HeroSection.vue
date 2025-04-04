a9ca424fc2ed90a3e3f2814ecda9a7a4312e7584
BAssal <baslm8496@gmail.com>
2025-04-04 15:12:31 +0200

    feat(home): add HeroSection component to HomeView
    Introduce a new HeroSection component to enhance the visual appeal of the home page. This component includes a hero banner with promotional content and styling for better user engagement.

---
+++ src/components/home/HeroSection.vue
@@ -0,0 +1,48 @@
+<template>
+    <section class="hero-section">
+        <div class="box">
+            <v-container>
+                <v-row>
+                    <v-col cols="6">
+                        <div class="content">
+                            <h3>
+                                <span>Huge Saving</span>
+                                on UHD Televisions
+                            </h3>
+                            <p class="text-white mb-3">Sale up to 70% off on selected items</p>
+                            <v-btn class="py-2 px-10 text-white" density="default" variant="outlined" style="border-radius: 30px; text-transform: none">Shop Now</v-btn>
+                        </div>
+                    </v-col>
+                </v-row>
+            </v-container>
+        </div>
+    </section>
+</template>
+<script>
+export default {};
+</script>
+<style lang="scss">
+.hero-section {
+    .box {
+        height: 600px;
+        background-image: url("../../assets/images/banner-bg.jpg");
+        background-size: cover;
+        background-position: -70px top;
+    }
+    .content {
+        padding: 180px 0 0 190px;
+        p {
+            font-size: 25px;
+        }
+        h3 {
+            font-size: 55px;
+            font-weight: 900;
+            text-transform: uppercase;
+            color: #fff;
+            span {
+                color: #11ffda;
+            }
+        }
+    }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,15 @@
 <template>
-    <h1>this is home page</h1>
+    <HeroSection />
 </template>
 
 <script>
 // @ is an alias to /src
+import HeroSection from "@/components/home/HeroSection.vue";
 
 export default {
     name: "HomeView",
+    components: {
+        HeroSection,
+    },
 };
 </script>
