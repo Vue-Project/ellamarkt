afdc790737ab95ef364bc1cdbe82d91ab8d1016f
BAssal <baslm8496@gmail.com>
2025-04-04 15:42:20 +0200

    feat(home): add features section with SVG icons
    Introduce a new features section to the home view, including three SVG icons for "Free Shipping & Returns", "Lowest Price Guarantee", and "Longest Warranties Offer". The icons are styled consistently and integrated into a responsive layout.

---
+++ src/components/home/TheFeatures.vue
@@ -0,0 +1,72 @@
+<template>
+    <section class="features">
+        <v-container>
+            <v-row>
+                <v-col>
+                    <div class="features-box">
+                        <CartTruck />
+                        <h5>Free Shipping & Returns</h5>
+                    </div>
+                </v-col>
+                <v-col>
+                    <div class="features-box lines">
+                        <LowestPrice />
+                        <h5>Lowest Price Guarantee</h5>
+                    </div>
+                </v-col>
+                <v-col>
+                    <div class="features-box">
+                        <LongestOffer />
+                        <h5>Longest Warranties Offer</h5>
+                    </div>
+                </v-col>
+            </v-row>
+        </v-container>
+    </section>
+</template>
+<script>
+import CartTruck from "../SVG/CartTruckIcon.vue";
+import LowestPrice from "../SVG/LowestPriceIcon.vue";
+import LongestOffer from "../SVG/LongestOfferIcon.vue";
+
+export default {
+    components: {
+        CartTruck,
+        LowestPrice,
+        LongestOffer,
+    },
+};
+</script>
+<style lang="scss">
+.features-box {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+    svg {
+        margin-right: 10px;
+    }
+    h5 {
+        font-size: 15px;
+        font-weight: 600;
+    }
+}
+.lines {
+    &::after {
+        content: "";
+        width: 2px;
+        height: 30px;
+        background-color: #dadada;
+        position: absolute;
+        right: 0;
+    }
+    &::before {
+        content: "";
+        width: 2px;
+        height: 30px;
+        background-color: #dadada;
+        position: absolute;
+        left: 0;
+    }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,18 @@
 <template>
     <HeroSection />
+    <TheFeatures />
 </template>
 
 <script>
 // @ is an alias to /src
 import HeroSection from "@/components/home/HeroSection.vue";
+import TheFeatures from "@/components/home/TheFeatures.vue";
 
 export default {
     name: "HomeView",
     components: {
         HeroSection,
+        TheFeatures,
     },
 };
 </script>
