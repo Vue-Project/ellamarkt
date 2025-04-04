07f6c8ba106a110c8de16447c77c04ed9ae2e39d
BAssal <baslm8496@gmail.com>
2025-04-04 21:10:28 +0200

    feat(home): add TopOffer component to HomeView
    Introduce a new TopOffer component to display a section of top offers on the homepage. This enhances the user experience by showcasing featured products with hover effects.

---
+++ src/components/home/TopOffers.vue
@@ -0,0 +1,58 @@
+<template>
+    <section class="top-offers">
+        <v-container>
+            <v-row>
+                <v-col cols="4">
+                    <v-hover v-slot="{ isHovering, props }">
+                        <div class="top-offers-box" v-bind="props">
+                            <img class="w-100" src="@/assets/images/playstations.webp" alt=" playstation image" :style="` transition: 0.7s all ease-out; scale:${isHovering ? 1.05 : 1}`" />
+                        </div>
+                    </v-hover>
+                </v-col>
+                <v-col cols="4">
+                    <v-hover v-slot="{ isHovering, props }">
+                        <div class="top-offers-box" v-bind="props">
+                            <img class="w-100" src="@/assets/images/headset.webp" alt=" Headset image" :style="` transition: 0.7s all ease-out; scale:${isHovering ? 1.05 : 1}`" />
+                        </div>
+                    </v-hover>
+                </v-col>
+                <v-col cols="4">
+                    <v-hover v-slot="{ isHovering, props }">
+                        <div class="top-offers-box" v-bind="props">
+                            <img class="w-100" src="@/assets/images/chairs.webp" alt=" chair image" :style="` transition: 0.7s all ease-out; scale:${isHovering ? 1.05 : 1}`" />
+                        </div>
+                    </v-hover>
+                </v-col>
+                <v-col cols="6">
+                    <v-hover v-slot="{ isHovering, props }">
+                        <div class="top-offers-box" v-bind="props">
+                            <img class="w-100" src="@/assets/images/laptops.webp" alt=" laptops image" :style="` transition: 0.7s all ease-out; scale:${isHovering ? 1.05 : 1}`" />
+                        </div>
+                    </v-hover>
+                </v-col>
+                <v-col cols="6">
+                    <v-hover v-slot="{ isHovering, props }">
+                        <div class="top-offers-box" v-bind="props">
+                            <img class="w-100" src="@/assets/images/iphones.webp" alt=" iphones image" :style="` transition: 0.7s all ease-out; scale:${isHovering ? 1.05 : 1}`" />
+                        </div>
+                    </v-hover>
+                </v-col>
+            </v-row>
+        </v-container>
+    </section>
+</template>
+<script>
+export default {};
+</script>
+<style lang="scss">
+.top-offers {
+    background-color: #f5f5f5;
+    padding: 50px 0;
+}
+.top-offers-box {
+    padding-right: 15px;
+    overflow: hidden;
+    cursor: pointer;
+    border-radius: 10px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,21 @@
 <template>
     <HeroSection />
     <TheFeatures />
+    <TopOffer />
 </template>
 
 <script>
 // @ is an alias to /src
 import HeroSection from "@/components/home/HeroSection.vue";
 import TheFeatures from "@/components/home/TheFeatures.vue";
+import TopOffer from "@/components/home/TopOffers.vue";
 
 export default {
     name: "HomeView",
     components: {
         HeroSection,
         TheFeatures,
+        TopOffer,
     },
 };
 </script>
