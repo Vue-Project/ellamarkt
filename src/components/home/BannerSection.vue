84ef9fc3487090ad8547b93d43e9bad2ffe73f77
BAssal <baslm8496@gmail.com>
2025-04-09 08:31:28 +0200

    feat(home): add BannerTV and BannerSection components
    Introduce new components to display banner images on the home page. BannerTV shows a single TV banner with hover effect, while BannerSection displays multiple banners in a grid layout with hover effects.

---
+++ src/components/home/BannerSection.vue
@@ -0,0 +1,28 @@
+<template>
+    <section>
+        <v-container>
+            <v-row>
+                <v-col cols="6" v-for="(banner, index) in banners" :key="index" style="cursor: pointer">
+                    <v-hover v-slot="{ isHovering, props }">
+                        <div class="pr-3" v-bind="props" :style="`transition: 0.4s all ease-out; scale:${isHovering ? 1.02 : 1}`">
+                            <img class="w-100" :src="banner.image" :alt="banner.alt" />
+                        </div>
+                    </v-hover>
+                </v-col>
+            </v-row>
+        </v-container>
+    </section>
+</template>
+<script>
+export default {
+    name: "BannerSection",
+    data() {
+        return {
+            banners: [
+                { image: require("@/assets/images/band-right-cover.webp"), alt: "banner right" },
+                { image: require("@/assets/images/band-left-cover.webp"), alt: "banner left" },
+            ],
+        };
+    },
+};
+</script>
---
+++ src/components/home/BannerTV.vue
@@ -0,0 +1,23 @@
+<template>
+    <section>
+        <v-container>
+            <v-row>
+                <v-col cols="12" style="cursor: pointer">
+                    <v-hover v-slot="{ isHovering, props }">
+                        <div v-bind="props" :style="`transition: 0.4s all ease-out; scale:${isHovering ? 1.02 : 1}`">
+                            <img class="w-100" :src="require('@/assets/images/tv-banner.webp')" alt="banner-tv" />
+                        </div>
+                    </v-hover>
+                </v-col>
+            </v-row>
+        </v-container>
+    </section>
+</template>
+<script>
+export default {
+    name: "BannerTV",
+    data() {
+        return {};
+    },
+};
+</script>
